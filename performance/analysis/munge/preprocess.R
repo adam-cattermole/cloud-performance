c5c7b1ed8bb325cf57bd2df6b6a87c38570a4d98
Adam Cattermole <a.d.cattermole@gmail.com>
2017-09-04 16:58:49 +0100

    Start at creating the new figures for next submission

--- performance/analysis/munge/preprocess.R
+++ performance/analysis/munge/preprocess.R
@@ -1,4 +1,6 @@
-data.azure = full.output[full.output$provider=='azure', names(full.output) != 'provider']
-data.aws = full.output[full.output$provider=='aws', names(full.output) != 'provider']
+data.azure = full.output[full.output$provider=='azure', names(full.output) != 'provider'] %>%
+  mutate(overall=gm.rowmeans(.[,4:14]))
+data.aws = full.output[full.output$provider=='aws', names(full.output) != 'provider'] %>%
+  mutate(overall=gm.rowmeans(.[,4:14]))
 data.azure = droplevels(data.azure)
 data.aws = droplevels(data.aws)
--- performance/analysis/src/analysis.R
+++ performance/analysis/src/analysis.R
@@ -4,14 +4,12 @@ load.project()
 ### AVERAGE DATA FRAMES ###
 
 data.average.azure = data.azure %>%
-  mutate(overall=gm.rowmeans(.[,4:14])) %>%
   group_by(vm_type, vm_size) %>%
   summarise_each(funs(mean), -iteration)
 
 data.average.azure
 
 data.average.aws = data.aws %>%
-  mutate(overall=gm.rowmeans(.[,4:14])) %>%
   group_by(vm_type, vm_size) %>%
   summarise_each(funs(mean), -iteration)
 
@@ -24,3 +22,51 @@ aws.specs %>% tbl_df()
 
 
 # TODO: ATTEMPT TO RECREATE PAPER GRAPHS WITH MORE CLARITY
+
+
+#### AZURE: Initial Reproduction of Graphics ####
+
+data.azure.basic = data.azure %>%
+  filter(as.character(vm_type) == "basic")
+
+data.azure.basic
+
+data.azure.standard = data.azure %>%
+  filter(as.character(vm_type) == "standard" & !endsWith(as.character(vm_size), "v2"))
+
+data.azure.standard
+  
+data.azure.standard.v2 = data.azure %>%
+  filter(as.character(vm_type) == "standard" & endsWith(as.character(vm_size), "v2"))
+
+data.azure.standard.v2
+
+
+ggplotSummaryCi(merge(data.azure.basic, azure.specs), title="Performance of Azure 'Basic' Instance Types")
+ggplotSummaryCi(merge(data.azure.standard, azure.specs), title="Performance of Azure 'Standard' Instance Types")
+ggplotSummaryCi(merge(data.azure.standard.v2, azure.specs), title="Performance of Azure 'Standard V2' Instance Types")
+
+
+#### EC2: Initial Reproduction of Graphics ####
+
+data.aws.t2 = data.aws %>%
+  filter(as.character(vm_type) == "t2")
+
+data.aws.t2
+
+data.aws.m4 = data.aws %>%
+  filter(as.character(vm_type) == "m4")
+
+data.aws.m4
+
+data.aws.c4 = data.aws %>%
+  filter(as.character(vm_type) == "c4")
+
+data.aws.c4
+
+## This sort of works, see summary.R in functions. The scaling is slightly off
+
+ggplotSummaryCi(merge(data.aws.t2, aws.specs), title="Performance of AWS t2 Instance Types")
+
+
+
